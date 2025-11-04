8b3bebf98e30b8df8a5f86b6fd6ed1c6360d8d4a
Scott Gorlin <scottgorlin@gmail.com>
2025-11-04 07:55:43 -0500

    improve talos reboot

--- reboot-talos.sh
+++ reboot-talos.sh
@@ -3,10 +3,15 @@
 CONTROLPLANE=("18" "123" "127")
 WORKERS=("112" "118" "119")
 
-read -p "Launch rolling reboot of nodes? <y/N> " prompt
+read -p "Launch rolling reboot of nodes? <y/N/cp=include control plane> " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
   echo "Here we gooooo!"
+  NODES=("${WORKERS[@]}")
+elif [[ $prompt == "cp" || $prompt == "CP" ]]
+then
+    echo "Including control plane nodes"
+    NODES=("${CONTROLPLANE[@]}" "${WORKERS[@]}")
 else
   echo "Exiting"
   exit 0
@@ -14,16 +19,9 @@ fi
 
 echo ""
 
-read -p "Include control plane? <y/N> " prompt
-if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-then
-  NODES=("${CONTROLPLANE[@]}" "${WORKERS[@]}")
-else
-  NODES=("${WORKERS[@]}")
-fi
-
 for ip in "${NODES[@]}"; do
   # Commands to be executed for each item
   echo "Rebooting 10.8.8.$ip"
+  sleep 5
   talosctl reboot -n 10.8.8.$ip --wait
 done
