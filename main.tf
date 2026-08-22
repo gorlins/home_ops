bef059f8223f51236ad745dc8af7117ac804b804
Scott Gorlin <scottgorlin@gmail.com>
2026-08-22 14:31:20 -0400

    providers for terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 terraform {
   required_version = "1.15.8"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.111.1"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.11.0"
+    }
+  }
+
   cloud {
 
     organization = "soybagel"
