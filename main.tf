218262287a6198b5f225da0cf47991747f38c461
Scott Gorlin <scottgorlin@gmail.com>
2026-08-29 20:49:02 -0400

    flexible terraform version, with sops

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 terraform {
-  required_version = "1.15.9"
+  required_version = "~> 1.15"
   required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.4"
+    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.111.1"
@@ -10,13 +14,16 @@ terraform {
       version = "0.11.0"
     }
   }
-
   cloud {
-
     organization = "soybagel"
-
     workspaces {
       name = "local"
     }
   }
+}
+
+provider "sops" {}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.enc.yaml"
 }
